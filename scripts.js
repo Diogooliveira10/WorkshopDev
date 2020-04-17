f02cb1649ec8f4b38a57f1eb762888c24bd670d9
Diogo Oliveira Vieira <55243757+Diogooliveira10@users.noreply.github.com>
2020-04-17 13:11:01 -0300

    Front-end

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-document
-    .querySelector("button.fat") // Pesquisa de um seletor "button.fat"
-    .addEventListener("click", function() { // adicionando um ouvidor de eventos "click"
+function onOff() { // adicionando um ouvidor de eventos "click"
         document
-            .querySelector("#footer")
+            .querySelector("#modal")
             .classList 
             .toggle("hide") // toggle colocar e tirar a class hide
-    }) 
+    }
+
+//document
+//    .querySelector("button.fat") // Pesquisa de um seletor "button.fat"
+//    .addEventListener("click", onOff) 
