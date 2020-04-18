d4b9f27ccfea045046ee9ad02ee9df39b6178c65
Diogo Oliveira Vieira <55243757+Diogooliveira10@users.noreply.github.com>
2020-04-18 13:40:55 -0300

    Front-end

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 function onOff() { // adicionando um ouvidor de eventos "click"
-        document
-            .querySelector("#modal")
-            .classList 
-            .toggle("hide") // toggle colocar e tirar a class hide
-    }
+    document
+        .querySelector("#modal")
+        .classList 
+        .toggle("hide") // toggle colocar e tirar a class hide
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+
+}
 
 //document
 //    .querySelector("button.fat") // Pesquisa de um seletor "button.fat"
