f52ede289b3af2227eb474aac73540206b8b65ee
Diogo Oliveira Vieira <55243757+Diogooliveira10@users.noreply.github.com>
2020-04-20 17:35:15 -0300

    finalizando front-end

--- server.js
+++ server.js
@@ -2,17 +2,82 @@
 const express = require('express')
 const server = express()
 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão em Família",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
+        title: "Pintura",
+        category: "Criatividade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
+        title: "Recortes",
+        category: "Criatividade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+        url: "https://rocketseat.com.br"
+    },
+]
+
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
+
+// configuração do nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
 
 // criei uma rota /
 // e capturo o pedido do cliente para responder
 server.get ("/", function(req, res) {
-    return res.sendFile(__dirname + "/index.html")
+
+    const reversedIdeas = [...ideas].reverse()
+
+    let lastIdeas = []
+    for (let idea of reversedIdeas) {
+        if(lastIdeas.length < 2) {
+            lastIdeas.push(idea)
+        }
+    }
+
+    return res.render("index.html", { ideas: lastIdeas })
 })
 
 server.get ("/ideias", function(req, res) {
-    return res.sendFile(__dirname + "/ideias.html")
+
+    const reversedIdeas = [...ideas].reverse()
+
+    return res.render("ideias.html", { ideas: reversedIdeas })
 })
 // ligueu meu servidor na porta 3000
 server.listen(3000)
