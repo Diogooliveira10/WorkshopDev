73f5f3676cc5a205bb6638c1e30bbf7c92c03ec3
Diogo Oliveira Vieira <55243757+Diogooliveira10@users.noreply.github.com>
2020-04-22 15:01:51 -0300

    Criando o banco de dados

--- db.js
+++ db.js
@@ -16,23 +16,23 @@ db.serialize(function() {
     `)
 
     // inserir dado na table
-    const query = `
-        INSERT INTO ideas(
-            image,
-            title,
-            category,
-            description,
-            link
-        ) VALUES (?,?,?,?,?);
-    `
+    // const query = `
+    //     INSERT INTO ideas(
+    //         image,
+    //         title,
+    //         category,
+    //         description,
+    //         link
+    //     ) VALUES (?,?,?,?,?);
+    // `
 
-    const values = [
-        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        "Cursos de Programação",
-        "Estudo",
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
-        "https://rocketseat.com.br"
-    ]
+    // const values = [
+    //     "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+    //     "Cursos de Programação",
+    //     "Estudo",
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea suscipit temporibus magni nihil iusto at provident quas aspernatur voluptates quasi unde in a est voluptas atque, possimus totam ad veniam?",
+    //     "https://rocketseat.com.br"
+    // ]
 
     // db.run(query, values, function(err) {
     //    if (err) return console.log(err)
@@ -40,8 +40,26 @@ db.serialize(function() {
     //    console.log(this)
     // })
 
-    // consultar dados na tabela
-
+    
     // deletar um dado na tabela
+    // DESAFIO: Implementar uma ideia de deletar uma "idea" do sistema
+
+    // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
+    //     if (err) return console.log(err)
+
+    //     console.log("DELETEI", this)
+    // })
+
+
+    // consultar dados na tabela
+    // db.all(`SELECT * FROM ideas`, function(err, rows) {
+    //     if (err) return console.log(err)
+
+    //     console.log(rows)
+    // })
+
+
 
 })
+
+module.exports = db
